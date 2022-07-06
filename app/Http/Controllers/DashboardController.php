6248d12758b77094fac3e8ee2664bd9934fce3e8
MinTheimKyaw <mintheimkyaw12@gmail.com>
2022-07-06 12:54:56 +0630

    add dashboard for item count and order count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Product;
+use App\Models\Order;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Dashboard/Index');
+        $productcount = Product::count();
+        $ordercount = Order::count();
+        return Inertia::render('Dashboard/Index',[
+            'productcount' => $productcount,
+            'ordercount' => $ordercount
+        ]);
     }
 }
--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -3,6 +3,33 @@
     <Head title="Dashboard" />
     <h1 class="mb-8 text-3xl font-bold">Dashboard</h1>
     <p class="mb-8 leading-normal">Welcome Dashboard</p>
+    
+    <section class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+      <div
+        class="flex items-center border border-transparent rounded-3xl p-5 bg-white"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mr-3" viewBox="0 0 20 20" fill="blue">
+          <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+        </svg>  
+        <div>
+          <div class="font-bold">Product Items</div>
+          <div class="text-md text-blue-600">{{productcount}} Items</div>
+        </div>
+      </div>
+
+      <div
+        class="flex items-center border border-transparent rounded-3xl p-5 bg-white"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mr-3" viewBox="0 0 20 20" fill="green">
+        <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+      </svg>
+        <div>
+          <div class="font-bold">Product Orders</div>
+          <div class="text-sm text-green-600">{{ordercount}} Orders</div>
+        </div>
+      </div>
+      
+    </section>
   </div>
 </template>
 
@@ -13,6 +40,10 @@ import Layout from '@/Shared/Layout'
 export default {
   components: {
     Head,
+  },
+  props: {
+    productcount: Number,
+    ordercount: Number,
   },
   layout: Layout,
 }
